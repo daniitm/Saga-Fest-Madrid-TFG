3e69552d1f760c0308094585c2ead46f5ec20f5d
Daniel Trivino <danieltrivinomarin@gmail.com>
2025-06-15 00:44:23 +0200

    Proyecto terminado 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,43 +21,5 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        // Default dates and turns
-        $defaultDates = [
-            '2025-07-25',
-            '2025-07-26',
-            '2025-07-27',
-        ];
-        $turnsData = [
-            [
-                'name' => 'Mañana',
-                'start_time' => '09:00',
-                'end_time' => '13:00',
-            ],
-            [
-                'name' => 'Tarde',
-                'start_time' => '16:00',
-                'end_time' => '20:00',
-            ],
-        ];
-        $defaultBreak = 15;
-
-        foreach ($defaultDates as $date) {
-            foreach ($turnsData as $turnData) {
-                $turn = Turn::firstOrCreate([
-                    'date' => $date,
-                    'name' => $turnData['name'],
-                ], [
-                    'start_time' => $turnData['start_time'],
-                    'end_time' => $turnData['end_time'],
-                ]);
-                Schedule::firstOrCreate([
-                    'turn_id' => $turn->id,
-                ], [
-                    'break' => $defaultBreak,
-                ]);
-            }
-        }
-    }
+ 
 }
